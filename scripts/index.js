507919e43ae7720c461a2306bcf88bdba1ec2ad0
Saif <ofaruq281@gmail.com>
2026-01-09 00:15:31 +0600

    added dynamic card for lets learn section using index.js

--- scripts/index.js
+++ scripts/index.js
@@ -18,7 +18,17 @@ const displayLevelWord=(words)=>{
     words.forEach(word=>{
         const card=document.createElement("div")
         card.innerHTML=`
-        <p>Cat</p>
+        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-5">
+            <h2 class="font-bold text-2xl">${word.word}</h2>
+            <p class="font-semibold">Meaning / pronunciation</p>
+
+            <div class="text-2xl font-medium font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+            <div class="flex justify-between items-center">
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+
+        </div>
         `
         wordContainer.appendChild(card)
     })

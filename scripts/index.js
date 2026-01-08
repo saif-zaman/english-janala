83e7df104f784598e441f59c52c9c9b78d808deb
Saif <ofaruq281@gmail.com>
2026-01-08 23:54:26 +0600

    created landing page and implemented some features using script.js

---
+++ scripts/index.js
@@ -0,0 +1,42 @@
+const loadLessons = () => {
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+        .then(res => res.json())
+        .then(json => displayLesson(json.data))
+}
+
+const loadLevelWord=(id)=>{
+    const url= `https://openapi.programming-hero.com/api/level/${id}`
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>displayLevelWord(data.data))
+}
+
+const displayLevelWord=(words)=>{
+    const wordContainer=document.getElementById("word-container")
+    wordContainer.innerHTML=""
+
+    words.forEach(word=>{
+        const card=document.createElement("div")
+        card.innerHTML=`
+        <p>Cat</p>
+        `
+        wordContainer.appendChild(card)
+    })
+}
+
+const displayLesson = (lessons) => {
+    const levelContainer = document.getElementById("level-container")
+    levelContainer.innerHTML = ""
+
+    for (let lesson of lessons) {
+        const btnDiv = document.createElement("div")
+        btnDiv.innerHTML = `
+        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"> 
+        <i class="fa-solid fa-circle-question"></i>Lesson - ${lesson.level_no}
+        </button>
+        `
+
+        levelContainer.append(btnDiv)
+    }
+}
+loadLessons()

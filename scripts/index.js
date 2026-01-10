dc475c3551a240e0818a96f49de4d42b9f76c186
Saif <ofaruq281@gmail.com>
2026-01-11 00:20:58 +0600

    added active feature and modal of info button

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,21 @@ const loadLessons = () => {
         .then(res => res.json())
         .then(json => displayLesson(json.data))
 }
+const removeActive=()=>{
+    const lessonBtns=document.querySelectorAll(".lesson-btn")
+    lessonBtns.forEach(btn=>btn.classList.remove("active"))
+}
 
 const loadLevelWord = (id) => {
     const url = `https://openapi.programming-hero.com/api/level/${id}`
     fetch(url)
         .then(res => res.json())
-        .then(data => displayLevelWord(data.data))
+        .then(data => {
+            removeActive()
+            const clickedBtn=document.getElementById(`lesson-btn-${id}`)
+            clickedBtn.classList.add("active")
+            displayLevelWord(data.data)
+        })
 }
 
 const displayLevelWord = (words) => {
@@ -34,7 +43,7 @@ const displayLevelWord = (words) => {
 
             <div class="text-2xl font-medium font-bangla">"${word.meaning?word.meaning:"অর্থ পাওয়া যায় নি"} / ${word.pronunciation ? word.pronunciation : "উচ্চারণ পাওয়া যায় নি"}"</div>
                 <div class="flex justify-between items-center">
-                    <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                    <button onclick="my_modal_5.showModal()" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
                     <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
                 </div>
 
@@ -51,8 +60,8 @@ const displayLesson = (lessons) => {
     for (let lesson of lessons) {
         const btnDiv = document.createElement("div")
         btnDiv.innerHTML = `
-        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"> 
-        <i class="fa-solid fa-circle-question"></i>Lesson - ${lesson.level_no}
+        <button id="lesson-btn-${lesson.level_no}"onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn"> 
+            <i class="fa-solid fa-circle-question"></i>Lesson - ${lesson.level_no}
         </button>
         `
 
